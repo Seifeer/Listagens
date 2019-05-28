9db80c72853b35dc06685fbd27aab5fb42c5809a
Ermeson16 <ermeson16000@gmail.com>
2019-05-28 17:18:03 -0300

    Recycler View learn

--- app/src/main/java/com/example/listagens/ComidasAdapter.java
+++ app/src/main/java/com/example/listagens/ComidasAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -23,7 +24,7 @@ public class ComidasAdapter extends RecyclerView.Adapter<ComidasAdapter.ComidaVi
     @Override
     public ComidaViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
 
-        LayoutInflater inflater=new LayoutInflater.from(context);
+        LayoutInflater inflater = new LayoutInflater.from(context);
         View view=inflater.inflate(R.layout.item_comida,viewGroup,false);
 
         ComidaViewHolder viewHolder=new ComidaViewHolder(view);
@@ -36,7 +37,7 @@ public class ComidasAdapter extends RecyclerView.Adapter<ComidasAdapter.ComidaVi
 
         comidaViewHolder.TxtComidaNome.setText(comida.getNome());
         comidaViewHolder.TxtComidaIngredientes.setText(comida.getIngredientes());
-        String valor = String.format("R$ %.2f",)
+        //String valor = String.format("R$ %.2f",)
         comidaViewHolder.TxtComidaValor.setText("R$ "+comida.getValor());
     }
 
@@ -58,6 +59,15 @@ public class ComidasAdapter extends RecyclerView.Adapter<ComidasAdapter.ComidaVi
             TxtComidaNome=itemView.findViewById(R.id.txt_comida_nome);
             TxtComidaIngredientes=itemView.findViewById(R.id.txt_xomida_ingredientes);
             TxtComidaValor=itemView.findViewById(R.id.txt_comida_valor);
+
+            // Configurar cliks
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public  void onClick (View v){
+                    int posicao=getAdapterPosition();
+                    Toast.make(v, "Clicou.", Toast.LENGTH_LONG).show();
+                }
+            });
         }
     }
 }
